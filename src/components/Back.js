bb11831bb89951365166fcfe924eaf5f2192e160
Angel <soporte@libus.com>
2022-11-14 14:02:02 -0300

    Se oculta boton de version mobile

--- src/components/Back.js
+++ src/components/Back.js
@@ -2,6 +2,6 @@ import { IoIosArrowBack } from 'react-icons/io'
 
 export default function Back(){
     return (
-        <button className="back-btn" onClick={() => window.history.back()}><IoIosArrowBack /></button>
+        <button className="back-btn" onClick={() => window.history.back()}><IoIosArrowBack size={25} /></button>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,30 @@
-import { FcSearch } from 'react-icons/fc';
 import { Link } from "react-router-dom";
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import styled from "styled-components";
+
+// Componente del boton del NavBar
+export const MobileIcon = styled.button`
+  display: none;
+  border: none;
+  width: 5rem;
+  background-color: transparent;
+  outline: none;
+  @media screen and (max-width: 800px) {
+    display: block;
+    cursor: pointer;
+  }
+`;
 
 export default function Nav(){
+    // Usado para desplegar el panel derecho en la version mobile
     const burgerRef = useRef();
     const navRef = useRef();
     const setBurger = () => {
         burgerRef.current.classList.toggle("active");
         navRef.current.classList.toggle("nav-active");
     }
+    // Efecto del boton del NavBar
     const navlinks = document.querySelectorAll('.navlinks li');
-
     navlinks.forEach((link,index) => {
         if (link.style.animation){
             link.style.animation='';
@@ -18,7 +32,8 @@ export default function Nav(){
             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1}s`;
         }
     });
-
+    // Comprobacion del estado del boton del NavBar
+    const [showMobileMenu, setShowMobileMenu] = useState(false)
     return (
         <div>
         <nav>
@@ -33,21 +48,15 @@ export default function Nav(){
             <li>
                 <Link to="/inyectoras">Inyectoras</Link>
             </li>
-            <li>
-                <ul className="navsearch">
-                    <div className="div-center">
-                        <input type="text" className="search-input" placeholder="Ingresar" />
-                        <button type="button" className="nav-btn"><FcSearch /></button>
-                    </div>
-                </ul>
-                <Link to="/search"><FcSearch /></Link>
-            </li>
         </ul>
-        <button ref={burgerRef} className="burger" onClick={setBurger} >
+        <MobileIcon ref={burgerRef} className="burger" onClick={() => {
+            setShowMobileMenu(!showMobileMenu)
+            setBurger()
+        }}>
             <span className="line"></span>
             <span className="line"></span>
             <span className="line"></span>
-        </button>
+        </MobileIcon>
     </nav>
     </div>
     )
