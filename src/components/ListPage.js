64715ca5c075873a69634a1c9d46a98824552ce2
Angel <soporte@libus.com>
2022-12-16 11:06:32 -0300

    Modificacion de componente ListPage

--- src/components/ListPage.js
+++ src/components/ListPage.js
@@ -1,22 +1,44 @@
 import Post from "./Post"
 import { useState, useEffect } from "react"
 import getPosts from '../api/axios';
+import '../styles/search.css'
+import SearchBar from '../components/SearchBar'
+import styled from "styled-components";
+import Form from '../lib/Form'
+import Input from '../lib/Input';
+
+export const MobileForm = styled.div`
+    justify-content: center;
+    align-items: center;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    @media screen and (max-width: 800px) {
+        grid-template-columns: 1fr;
+    }
+`;
 
 const ListPage = () => {
+    const [posts, setPosts] = useState([])
     const [searchResults, setSearchResults] = useState([])
     const [loading,setLoading]=useState(true)
+    let content;
 
     useEffect(() => {
-    getPosts().then(json => {
+        getPosts().then(json => {
+        setPosts(json)
         setSearchResults(json)
         setLoading(false)
     })
     }, [])
 
-    const results = searchResults.data && searchResults.data.map(post => <Post key={post._id} post={post}/>);
-    console.log(searchResults)
-    const content = results ? results : <article><p>No Matching Posts</p></article>
-    
+    const results = searchResults && searchResults.map(post => <Post key={post._id} post={post}/>);
+    console.log("LISTPAGE>>",searchResults)
+    if (searchResults[60] || !searchResults[1]) {
+        content = <tr><td><article><p>No Matching Posts</p></article></td></tr>
+    } else {
+        content = results
+    }
+
    const cargarElementos = async ()=>{
 
    try{
@@ -48,58 +70,60 @@ cargarElementos()
 
 if (loading) return <h1>LOADING...</h1>
     return (
-        <div>
-            <table>
-            <thead>
-                <tr>
-                    <th>Numero de operacion</th>
-                    <th>codigo del producto</th>
-                    <th>Numero de Inyectora</th>
-                    <th>Numero de Molde</th>
-                    <th>Tiempo de ciclo</th>
-                    <th>Materia Prima</th>
-                </tr>
-            </thead> 
-            <tbody>{content}</tbody>
+        <div className="main-container">
+            <div className="div-fecha">
+                <SearchBar posts={posts} setSearchResults={setSearchResults} />
+            </div>
+            <table className="table-cls">
+                <thead>
+                        <tr>
+                            <th>Numero de operacion</th>
+                            <th>Codigo del producto</th>
+                            <th>Numero de Inyectora</th>
+                            <th>Numero de Molde</th>
+                            <th>Tiempo de ciclo</th>
+                            <th>Materia Prima</th>
+                        </tr>
+                </thead>
+                <tbody>
+                    {content}
+                </tbody>
+            </table>
+
+            <br/>
             
-        </table>
-
-        
-
-        <br/>
-        
-        <form name="formulario">
-            <div>
-                <br/><label>Numero de operacion</label><br/>
-                <input type="text" id="numero_de_operacion"  />
+            <Form title="Formulario" action="#" name="formulario" 
+            classButton="div-enviar" nameButton="Agregar" >
+            <MobileForm>
+            <Input title="Numero de Operacion" id="numero_de_operacion" placeholder="Ingresar" required="required" />
+            <Input title="Numero de Producto" id="codigo_de_producto" placeholder="Ingresar" required="required" />
+            <Input title="Materia Prima" id="materia_prima" placeholder="Ingresar" />
+            <Input title="Numero de Inyectorra" id="numero_de_inyectora" placeholder="Ingresar" required="required" />
+            <Input title="Numero de Molde" id="numero_de_molde" placeholder="Ingresar" required="required" />
+            <Input title="Tiempo de ciclo" id="tiempo_de_ciclo" placeholder="Ingresar" required="required" />
+            <Input title="T. de ciclo optimo" id="" placeholder="Ingresar" required="required" />
+            <Input title="T. de inyección" id="" placeholder="Ingresar" required="required" />
+            <Input title="T. de extrusor" id="" placeholder="Ingresar" required="required" />
+            <Input title="T. de enfriamiento" id="" placeholder="Ingresar" required="required" />
+            <Input title="Cojin" id="" placeholder="Ingresar" required="required" />
+            <Input title="Pico de preción" id="" placeholder="Ingresar" required="required" />
+            <Input title="Punto de transferencia" id="" placeholder="Ingresar" required="required" />
+            <Input title="Fuerza de cierre" id="" placeholder="Ingresar" required="required" />
+            <Input title="Tamaño dosificado" id="" placeholder="Ingresar" required="required" />
+            <Input title="Contrapresión" id="" placeholder="Ingresar" required="required" />
+            <Input title="Presión de enpaque" id="" placeholder="Ingresar" required="required" />
+            <Input title="Tiempo de retención" id="" placeholder="Ingresar" required="required" />
+            <Input title="T. de expulsion" id="" placeholder="Ingresar" required="required" />
+            {/* <Select title="Materia Prima" >
+            <option value="op1">Estandar</option>
+            <option value="op2">Boq. Cobre</option>
+            <option value="op3">Valv. A 2</option>
+            <option value="op4">Otro</option>
+            <option value="op5">Diametro</option>
+            </Select> */}
+            </MobileForm>
+            </Form>
             </div>
-            <div>
-                <br/><label>Codigo de producto</label><br/>
-                <input type="text" id="codigo_de_producto" />
-            </div>
-            <div>
-                <br/><label>Numero de inyectora</label><br/>
-                <input type="text" id="numero_de_inyectora" />
-            </div>
-            <div>
-                <br/><label>Numero de molde</label><br/>
-                <input type="text" id="numero_de_molde" />
-            </div>
-            <div>
-                <br/><label>Tiempo de ciclo</label><br/>
-                <input type="text" id="tiempo_de_ciclo" />
-            </div>
-            <div>
-                <br/><label>Materia prima </label><br/>
-                <input type="text" id="materia_prima" />
-               
-            </div> 
-            <button id="agregar">Agregar</button>
-            <div id="dinamic"></div>
-
-        
-        </form>
-        </div>
        
     )
 }
